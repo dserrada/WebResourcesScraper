4c9457c2cc441da6b319dea62bbf86b1afcd1e65
David Pérez Serrada <dserrada@gmail.com>
2021-05-09 19:03:54 +0200

    Fix a tests

--- src/test/java/org/terra/incognita/utils/torrent/scraper/WebResourcesScraperTest.java
+++ src/test/java/org/terra/incognita/utils/torrent/scraper/WebResourcesScraperTest.java
@@ -1,13 +1,11 @@
 package org.terra.incognita.utils.torrent.scraper;
 
-import org.apache.logging.log4j.core.util.Assert;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.io.IOException;
 import java.io.StringReader;
-
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -26,7 +24,7 @@ class WebResourcesScraperTest {
         final String test = "one";
         assertDoesNotThrow( () -> {
             WebResourceURL[] found = WebResourcesScraper.extractURLFromReader(new StringReader(test));
-            Assert.isEmpty(found);
+            assertTrue(Arrays.asList(found).isEmpty());
         });
     }
 
@@ -35,7 +33,8 @@ class WebResourcesScraperTest {
         final String test = "<td><a  class=\"text-white bg-primary rounded-pill d-block shadow-sm text-decoration-none my-1 py-1\" style=\"font-size: 18px; font-weight: 500;\" href='//blazing.network/torrents/series/150138_-1619837733-La-Costa-De-Los-Mosquitos---Temporada-1--HDTV-720p-AC3-5-2.torrent' download>Descargar</a></td>";
         assertDoesNotThrow( () -> {
             WebResourceURL[] found = WebResourcesScraper.extractURLFromReader(new StringReader(test));
-            Assert.isEmpty(found);
+            assertFalse(Arrays.asList(found).isEmpty());
+            assertEquals(found.length,1);
         });
     }
 
