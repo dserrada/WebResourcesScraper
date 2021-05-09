497fc9b0012502daaf2d3dfddb48f82b8a6e26be
David Pérez Serrada <dserrada@gmail.com>
2021-05-09 18:31:54 +0200

    First working version of extract

--- src/main/java/org/terra/incognita/utils/torrent/scraper/WebResourcesScraper.java
+++ src/main/java/org/terra/incognita/utils/torrent/scraper/WebResourcesScraper.java
@@ -14,9 +14,9 @@ import org.apache.logging.log4j.*;
  */
 public class WebResourcesScraper {
 
-    public static final String RESOURCE_URL_END_TOKEN = ".torrent\"";
-    public static final String URL_END_TOKEN = "\"";
-    public static final String URL_BEGIN_TOKEM = "\"https://";
+    public static final String RESOURCE_URL_END_TOKEN = ".torrent'"; // attribute delimiter could be " or '
+    public static final String URL_END_TOKEN = "'";
+    public static final String [] URL_BEGIN_TOKEMS  = { "'http://", "'https://","'//"};
     public static Logger log = LogManager.getLogger(WebResourcesScraper.class);
 
     public static WebResourceURL[]  extractURLFromReader(Reader reader) throws IOException {
@@ -25,31 +25,39 @@ public class WebResourcesScraper {
         String line = null;
         int index = 0;
         while ( (line = breader.readLine()) != null ) {
-            // Fist we check if there is a borrent url
+            // Fist we check if there is a torrent url
             if ( ( index = line.indexOf(RESOURCE_URL_END_TOKEN)) != -1 ) {
+                // FIXME: Change to log
+                System.out.println("index: " + index);
                 // We can find at least one resource url, or maybe more....
-                while ( index < line.length() ) {
-                    int beginIndex = line.indexOf(URL_BEGIN_TOKEM,index);
+                int beginIndex = -1;
+                int beginTokenLength = -1;
+                while ( index+RESOURCE_URL_END_TOKEN.length() <= line.length() ) {
+                    for(String token: URL_BEGIN_TOKEMS) {
+                        if ( (beginIndex=line.lastIndexOf(token,index)) != -1 ) {
+                            beginTokenLength = token.length();
+                            break;
+                        }
+                    };
+                    // FIXME: Change to log
+                    System.out.println("beginIndex: " + beginIndex);
                     if ( beginIndex != -1 ) {
-                        int endIndex = line.indexOf(URL_END_TOKEN, index + URL_BEGIN_TOKEM.length());
-                        if ( endIndex != -1 ) {
-                            if (line.substring(beginIndex, endIndex).endsWith(RESOURCE_URL_END_TOKEN)) {
-                                // Found an url torrent
-                                String url = line.substring(beginIndex + 1, endIndex - 1); // Remove the quotes
-                                WebResourceURL resource = new WebResourceURL(url);
-                                result.add(resource);
-                                index = endIndex+1;
-                            } else {
-                                // It isn't an url resource
-                                index++;
-                            }
+                        int endIndex = index + RESOURCE_URL_END_TOKEN.length();
+
+                        System.out.println(" beginIndex: " + beginIndex + " endIndex: " + endIndex);
+                        if (line.substring(beginIndex, endIndex).endsWith(RESOURCE_URL_END_TOKEN)) {
+                            // Found an url torrent
+                            String url = line.substring(beginIndex + 1, endIndex - 1); // Remove the quotes
+                            WebResourceURL resource = new WebResourceURL(url);
+                            result.add(resource);
+                            index = endIndex+1;
                         } else {
-                            // There isn't end of the url
+                            // It isn't an url resource
                             index++;
                         }
                     } else {
                         // No begin url, so no more urls
-                        continue; // Next line
+                        break; // Next line
                     }
                 }
             }
--- src/test/java/org/terra/incognita/utils/torrent/scraper/WebResourcesScraperTest.java
+++ src/test/java/org/terra/incognita/utils/torrent/scraper/WebResourcesScraperTest.java
@@ -1,5 +1,6 @@
 package org.terra.incognita.utils.torrent.scraper;
 
+import org.apache.logging.log4j.core.util.Assert;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -7,14 +8,10 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import java.io.StringReader;
 
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class WebResourcesScraperTest {
-
-    private String [] testHTML =  {
-            "one",
-            "<td><a  class=\"text-white bg-primary rounded-pill d-block shadow-sm text-decoration-none my-1 py-1\" style=\"font-size: 18px; font-weight: 500;\" href='//blazing.network/torrents/series/150138_-1619837733-La-Costa-De-Los-Mosquitos---Temporada-1--HDTV-720p-AC3-5-2.torrent' download>Descargar</a></td>"
-    } ;
 
     @BeforeEach
     void setUp() {
@@ -25,14 +22,21 @@ class WebResourcesScraperTest {
     }
 
     @Test
-    void extractURLFromReader() {
-        for(String u: testHTML) {
-            try {
-                WebResourceURL[] found = WebResourcesScraper.extractURLFromReader(new StringReader(u));
-                System.out.println("Urls#: " + found.length);
-            } catch ( IOException ioe ) {
-                ioe.printStackTrace();;
-            }
-        }
+    void extractURLFromReader1() {
+        final String test = "one";
+        assertDoesNotThrow( () -> {
+            WebResourceURL[] found = WebResourcesScraper.extractURLFromReader(new StringReader(test));
+            Assert.isEmpty(found);
+        });
     }
+
+    @Test
+    void extractURLFromReader2() {
+        final String test = "<td><a  class=\"text-white bg-primary rounded-pill d-block shadow-sm text-decoration-none my-1 py-1\" style=\"font-size: 18px; font-weight: 500;\" href='//blazing.network/torrents/series/150138_-1619837733-La-Costa-De-Los-Mosquitos---Temporada-1--HDTV-720p-AC3-5-2.torrent' download>Descargar</a></td>";
+        assertDoesNotThrow( () -> {
+            WebResourceURL[] found = WebResourcesScraper.extractURLFromReader(new StringReader(test));
+            Assert.isEmpty(found);
+        });
+    }
+
 }
