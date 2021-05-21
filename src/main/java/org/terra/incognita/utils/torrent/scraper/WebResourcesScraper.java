ace7278e00ffc71920785c5092c5f7a006b36699
David Pérez Serrada <dserrada@gmail.com>
2021-05-22 00:19:06 +0200

    Added the download of resources

--- src/main/java/org/terra/incognita/utils/torrent/scraper/WebResourcesScraper.java
+++ src/main/java/org/terra/incognita/utils/torrent/scraper/WebResourcesScraper.java
@@ -1,19 +1,22 @@
 package org.terra.incognita.utils.torrent.scraper;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.Reader;
-import java.util.ArrayList;
+import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
-import java.util.concurrent.ConcurrentSkipListSet;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.stream.Stream;
 
+import org.apache.commons.io.FileUtils;
 import org.apache.logging.log4j.*;
 
 /**
  * Extract URL torrent links from a Reader.
  *
- * <p>An url couldn't extends over several lines</p>
+ * <p>Hipotesis:  An url couldn't extends over several lines</p>
  */
 public class WebResourcesScraper {
 
@@ -22,9 +25,16 @@ public class WebResourcesScraper {
     public static final String [] URL_BEGIN_TOKEMS  = { "'http://", "'https://","'//"};
     public static Logger log = LogManager.getLogger(WebResourcesScraper.class);
 
-    public static WebResourceURL[]  extractURLFromReader(Reader reader) throws IOException {
+    public static List<WebResourceURL>  extractURLFromReader(Reader reader, boolean download, String destDirPath)
+            throws IOException {
         List<WebResourceURL> result = new CopyOnWriteArrayList<>();
         BufferedReader breader = new BufferedReader(reader);
+
+        File destDir = new File(destDirPath);
+        if ( destDir == null || !destDir.isDirectory() ) {
+            System.out.println("No existe el directorio destino");
+            return result;
+        }
 
         breader.lines().parallel().forEach( line -> {
                 int index = 0;
@@ -53,6 +63,16 @@ public class WebResourcesScraper {
                                 String url = line.substring(beginIndex + 1, endIndex - 1); // Remove the quotes
                                 WebResourceURL resource = new WebResourceURL(url);
                                 result.add(resource);
+                                if ( download ) {
+                                    // I need to extract the filename
+                                    String filename = url.substring(url.lastIndexOf("/")+1,url.length());
+                                    System.out.println("https:" + resource.getUrl().trim() + (destDirPath + File.separator + filename));
+                                    URL uurl = null;
+                                    try {
+                                        uurl = new URL("https:" + resource.getUrl().trim());
+                                        FileUtils.copyURLToFile(uurl, new File(destDirPath + File.separator + filename));
+                                    } catch ( IOException ioe) {};
+                                }
                                 index = endIndex+1;
                             } else {
                                 // It isn't an url resource
@@ -65,7 +85,41 @@ public class WebResourcesScraper {
                     }
                 }
             });
-        return (WebResourceURL[] )result.toArray( new WebResourceURL[result.size()]);
+        return result;
     }
 
+    public static List<WebResourceURL>  extractURLFromReader(Reader reader) throws IOException {
+        return extractURLFromReader(reader,false,null);
+    }
+
+        /**
+         * Extract resource urls from text files (mainly html files) in a directory
+         *
+         * @param dirPath   Teh path of the directory to scan.
+         * @param download  If the resources extracted must be downloaded
+         * @return An array of resource urls
+         *
+         * @throws IOException
+         */
+    public static List<WebResourceURL>  extractURLFromDir(String dirPath, boolean download, String destDirPath) throws IOException {
+        List<WebResourceURL> result = new CopyOnWriteArrayList<>();
+        try (Stream<Path> walk = Files.walk(Paths.get(dirPath))) {
+            walk.parallel().filter(Files::isRegularFile).forEach( file -> {
+                FileReader f = null;
+                System.out.println(" Escaneando el archivo: " + file.toString());
+                try {
+                    f = new FileReader(file.toFile());
+                    if ( f != null ) {
+                        result.addAll(WebResourcesScraper.extractURLFromReader(f,download, destDirPath));
+                    }
+                } catch ( IOException ioe ) {};
+            });
+        }
+        return result;
+    }
+
+    public static List<WebResourceURL>  extractURLFromDir(String dirPath) throws IOException {
+        return extractURLFromDir(dirPath,false,null);
+
+    }
 }
--- src/test/java/org/terra/incognita/utils/torrent/scraper/WebResourcesScraperTest.java
+++ src/test/java/org/terra/incognita/utils/torrent/scraper/WebResourcesScraperTest.java
@@ -5,11 +5,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.*;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+
 import java.util.Arrays;
-import java.util.stream.Stream;
+import java.util.List;
+
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -27,7 +26,7 @@ class WebResourcesScraperTest {
     void extractURLFromReaderString_1() {
         final String test = "one";
         assertDoesNotThrow( () -> {
-            WebResourceURL[] found = WebResourcesScraper.extractURLFromReader(new StringReader(test));
+            List<WebResourceURL> found = WebResourcesScraper.extractURLFromReader(new StringReader(test));
             assertTrue(Arrays.asList(found).isEmpty());
         });
     }
@@ -36,9 +35,9 @@ class WebResourcesScraperTest {
     void extractURLFromReaderString_2() {
         final String test = "<td><a  class=\"text-white bg-primary rounded-pill d-block shadow-sm text-decoration-none my-1 py-1\" style=\"font-size: 18px; font-weight: 500;\" href='//blazing.network/torrents/series/150138_-1619837733-La-Costa-De-Los-Mosquitos---Temporada-1--HDTV-720p-AC3-5-2.torrent' download>Descargar</a></td>";
         assertDoesNotThrow( () -> {
-            WebResourceURL[] found = WebResourcesScraper.extractURLFromReader(new StringReader(test));
+            List<WebResourceURL> found = WebResourcesScraper.extractURLFromReader(new StringReader(test));
             assertFalse(Arrays.asList(found).isEmpty());
-            assertEquals(found.length,1);
+            assertEquals(found.size(),1);
         });
     }
 
@@ -46,9 +45,9 @@ class WebResourcesScraperTest {
     void extractURLFromReaderFile_1() {
         assertDoesNotThrow( () -> {
             Reader fileReader = new InputStreamReader(getClass().getClassLoader().getResourceAsStream("Saga-James-Bond"));
-            WebResourceURL[] found = WebResourcesScraper.extractURLFromReader(fileReader);
+            List<WebResourceURL> found = WebResourcesScraper.extractURLFromReader(fileReader);
             assertFalse(Arrays.asList(found).isEmpty());
-            assertEquals(found.length,1);
+            assertEquals(found.size(),1);
             for(WebResourceURL resource : found) {
                 System.out.println("url: " + resource.getUrl());
             }
@@ -58,21 +57,9 @@ class WebResourcesScraperTest {
     @Test
     void extractURLFromReaderDir_1() {
         System.out.println("Entrando");
+        final String dir = "/home/john/dontorrents.one";
         assertDoesNotThrow( () -> {
-            try (Stream<Path> walk = Files.walk(Paths.get("/home/john/dontorrents.one"))) {
-                walk.parallel().filter(Files::isRegularFile).forEach( file -> {
-                    FileReader f = null;
-                    try {
-                        f = new FileReader(file.toFile());
-                        if ( f != null ) {
-                            WebResourceURL[] found = WebResourcesScraper.extractURLFromReader(f);
-                            for(WebResourceURL resource : found) {
-                                System.out.println(resource.getUrl());
-                            }
-                        }
-                    } catch ( IOException ioe ) {};
-                });
-            }
+            WebResourcesScraper.extractURLFromDir(dir,true,"./downloads");
         });
 
     }
